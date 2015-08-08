764a19e3de8a1df50dc1604403dd49b35355ae24
JenittaMary <beaulahjeni@gmail.com>
2015-08-08 11:08:30 +0530

    Addition

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x,y,z;
+	System.out.println("Enter The Two numbers:");
+Scanner  in=new Scanner(System.in);
+ x= in.nextInt();
+y=in.nextInt();
+z=x+y;
+System.out.println(" Addition is:"+z);
+in.close();
+
+	}
+
+}
