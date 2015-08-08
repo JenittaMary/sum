3b353cbbb6b61d8acd363bb9f1947fbc70f2437e
Vignesh <rajvicky2893@gmail.com>
2015-08-08 11:54:24 +0530

    Vicky added a line

--- Add.java
+++ Add.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class Add {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int x,y,z;
-	System.out.println("Enter The Two numbers:");
-Scanner  in=new Scanner(System.in);
- x= in.nextInt();
-y=in.nextInt();
-z=x+y;
-System.out.println(" Addition is:"+z);
-System.out.println(" Thank You");
-in.close();
+		int x, y, z;
+		System.out.println("Enter The Two 
 
+numbers:");
+		Scanner in = new Scanner(System. in );
+		x = in .nextInt();
+		y = in .nextInt();
+		z = x + y;
+		System.out.println(" Addition is:" + z);
+		System.out.println(" Thank You"); in .close();
+		System.out.println(" I added a new line here");
 	}
 
 }
